0b121c38cf3711c0c3f7135f154bf36d010ec603
chaya <chayavogel1@gmail.com>
2023-11-14 19:25:24 -0500

    completes lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -=1
+    if i == 0:
+        print("Happy New Year!")
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_integers = []
+    for integer in int_list:
+        squared_integer = integer * integer
+        squared_integers.append(squared_integer)
+    return squared_integers
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 1
+    while i < 101:
+        if (i% 3 == 0) and (i% 5 == 0):
+            print("FizzBuzz")
+        elif (i% 3 == 0):
+            print("Fizz")
+        elif (i% 5 == 0):
+            print("Buzz")
+        else:
+            print(i)
+        i += 1
+
+fizzbuzz()
